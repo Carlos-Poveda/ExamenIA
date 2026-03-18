6a3805e7cb785e01378f89e273794793de543d78
Carlos <cpovedasanjose@gmail.com>
2026-03-18 10:29:01 +0100

    completo

--- src/main/java/org/example/examenspringboot/controller/ItemController.java
+++ src/main/java/org/example/examenspringboot/controller/ItemController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("/")
